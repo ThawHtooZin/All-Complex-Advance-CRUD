2f82648118151e614097b7d8706d6822d49ba9a0
remote <remote>
2023-06-08 21:08:46 +0630

    finished the advance project with pagination

---
+++ 4-dropdown-and-search-and-pagination-advance-CRUD/crud-projects-db.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 08, 2023 at 09:53 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud-projects-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advance1_crud`
+--
+
+CREATE TABLE `advance1_crud` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `advance1_crud`
+--
+
+INSERT INTO `advance1_crud` (`id`, `username`, `password`, `email`, `role`) VALUES
+(1, 'User', 'pass', 'user@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic_crud`
+--
+
+CREATE TABLE `basic_crud` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `basic_crud`
+--
+
+INSERT INTO `basic_crud` (`id`, `username`, `password`, `email`) VALUES
+(4, 'User4', 'pass', 'user4@gmail.com'),
+(5, 'User5', 'pass', 'user5@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `advance1_crud`
+--
+ALTER TABLE `advance1_crud`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic_crud`
+--
+ALTER TABLE `basic_crud`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `advance1_crud`
+--
+ALTER TABLE `advance1_crud`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `basic_crud`
+--
+ALTER TABLE `basic_crud`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
