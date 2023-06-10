5cc58a36b674bf0a2c8149ddd0be217ee2fccc8d
remote <remote>
2023-06-10 20:46:26 +0630

    finished the crud that can filter the data with date

---
+++ 8-crud-with-user-registering-with-date-filter-interface/index.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <body>
+    <?php include 'navbar.php'; ?>
+    <div class="container mt-5">
+      <h1 class="text-center bg-dark text-light">Welcome</h1>
+    </div>
+  </body>
+</html>
