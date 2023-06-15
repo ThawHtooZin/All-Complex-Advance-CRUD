b6f355c809bc5d8f71cc5e4ff016752080836f13
remote <remote>
2023-06-15 13:17:22 +0630

    added the menu file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <body>
+    <div class="container">
+      <h1>CRUD Proojects</h1>
+      <div class="list-group">
+        <a href="1-Basic-CRUD/" class="list-group-item list-group-item-action list-group-item-dark">1-Basic-CRUD</a>
+        <a href="2-dropdown-advance-CRUD/" class="list-group-item list-group-item-action list-group-item-dark">2-dropdown-advance-CRUD</a>
+        <a href="3-dropdown-and-search-advance-CRUD/" class="list-group-item list-group-item-action list-group-item-dark">3-dropdown-and-search-advance-CRUD</a>
+        <a href="4-dropdown-and-search-and-pagination-advance-CRUD/" class="list-group-item list-group-item-action list-group-item-dark">4-dropdown-and-search-and-pagination-advance-CRUD/a>
+        <a href="5-dropdown-and-search-and-pagination-inputvalidation-advance-CRUD0/" class="list-group-item list-group-item-action list-group-item-dark">5-dropdown-and-search-and-pagination-inputvalidation-advance-CRUD0</a>
+        <a href="6-dropdown-and-search-and-pagination-inputvalidation-csrftoken-advance-CRUD/" class="list-group-item list-group-item-action list-group-item-dark">6-dropdown-and-search-and-pagination-inputvalidation-csrftoken-advance-CRUD</a>
+        <a href="7-crud-with-user-registering-interface/" class="list-group-item list-group-item-action list-group-item-dark">7-crud-with-user-registering-interface</a>
+        <a href="8-crud-with-user-registering-with-date-filter-interface/" class="list-group-item list-group-item-action list-group-item-dark">8-crud-with-user-registering-with-date-filter-interface</a>
+        <a href="9-crud-with-export-the-database-into-excel-file/" class="list-group-item list-group-item-action list-group-item-dark">9-crud-with-export-the-database-into-excel-file</a>
+      </div>
+    </div>
+  </body>
+</html>
